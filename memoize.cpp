f1218c4dc8f7a6f6138cdfbc293d1c41669f31f3
atherfawaz <atherfawaz@users.noreply.github.com>
2019-06-08 18:57:55 +0500

    Minor bug fixes

--- memoize.cpp
+++ memoize.cpp
@@ -4,6 +4,7 @@ using namespace std;
 long long int global[100];
 
 long long int fib(int n) {
+    cout << "Current N: " << n << endl;
     if (n == 0 || n == 1) {
         return global[n];
     }
@@ -11,26 +12,34 @@ long long int fib(int n) {
         if (global[n - 1] == 0 && global[n - 2] == 0) {
             long long int result = fib(n-1) + fib(n-2);
             global[n] = result;
-            cout << "Fib(" << n << "): " << result << " (Result)" << endl;
+            cout << "(NA)" << endl;
             return result;
         }
-        else if (global[n - 1] == 0) {
-            long long int result = global[n - 2] + fib(n - 1);
-            global[n - 1] = result;
+        else if (global[n - 2] == 0 && global[n - 1] != 0) {
+            long long int result = global[n - 1] + fib(n - 2);
+            global[n - 2] = result;
             cout << "(AA-1)" << endl;
             return result;
         }
-        else if (global[n - 2] == 0) {
-            long long int result = global[n - 1] + fib(n - 2);
-            global[n - 2] = result;
+        else if (global[n - 1] == 0 && global[n - 2] != 0) {
+            long long int result = global[n - 1] + fib(n - 1);
+            global[n - 1] = result;
             cout << "(AA-2)" << endl;
             return result;
         }
         else if (global[n - 2] != 0 && global[n - 1] != 0) {
             long long int result = global[n - 1] + global[n - 2];
+            global[n] = result;
             cout << "(AA-BC)" << endl;
             return result;
         }
+    }
+}
+
+void printGlobal() {
+    cout << endl;
+    for (int i = 0; i < 100; i++) {
+        cout << global[i] << " ";
     }
 }
 
@@ -40,6 +49,7 @@ int main () {
         global[i] = 0;
     }
     int x = 5;
-    fib(x);
+    cout << "Fib(" << x << "): " << endl << fib(x) << endl;
+    printGlobal();
     return 0;
 }
